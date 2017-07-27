dc9c3be6ed90f6524e47ec599243ba2a638234ce
Justin Gagne <gagnej3@wit.edu>
2017-07-26 21:59:33 -0400

    Incorperating Accord.NET, added movement patterns.

---
+++ Assets/Scripts/AudioMachineLearning.cs
@@ -0,0 +1,230 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using Accord;
+using Accord.MachineLearning;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using Accord.Audio;
+using Accord.Audio.Formats;
+using Accord.MachineLearning;
+using Accord.MachineLearning.DecisionTrees;
+using Accord.MachineLearning.Performance;
+using Accord.Math;
+using Accord.Math.Distances;
+using Accord.Math.Optimization.Losses;
+using Accord.Statistics;
+using Accord.Statistics.Kernels;
+using NAudio;
+using NAudio.Wave;
+
+
+class AudioMachineLearning : MonoBehaviour
+{
+    public Text Screen;
+    private const string DESIRED_AUDIO_PATH = "/Users/gagnej3/School/4th_Year/Summer_2017/Senior_Design/Audio/Desired";
+    private const string UNDESIRED_AUDIO_PATH = "/Users/gagnej3/School/4th_Year/Summer_2017/Senior_Design/Audio/Undesired";
+    private readonly int DESIRED = 1;
+    private readonly int UNDESIRED = 0;
+    private string _guiText = "";
+
+    private const string TableName = "Audio Table";
+    private const string ColumnRootMeanSquare = "Root Mean Square";
+    private const string ColumnMax = "Max Peak Value";
+    
+    private static float TRAINING_PERCENTAGE = 80;
+    
+    // Tuple arrays for storing the decoded Signal object and the information obtained from the .wav file
+    private Accord.Tuple<WaveStream, FileInfo>[] _desiredData;
+    private Accord.Tuple<WaveStream, FileInfo>[] _undesiredData;
+
+    private WaveStream[] _desiredAudio;
+    private WaveStream[] _undesiredAudio;
+    
+    private List<int> mTrainingOutput = new List<int>();
+    private List<double[]> mTrainingInput = new List<double[]>();
+    private List<int> mTestOutput = new List<int>();
+    private List<double[]> mTestInput = new List<double[]>();
+
+    private RandomForest _forest;
+    private int _trainingIndex = 0;
+    private int _correctClassification = 0;
+    
+    private void Start()
+    {
+        Run();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Classify();
+        }
+    }
+
+    private void Classify()
+    {
+        
+        int prediction = -1;
+        int expected = -2;
+
+        if (_trainingIndex < mTestInput.Count)
+        {
+           prediction =  _forest.Decide(mTestInput[_trainingIndex]);
+           expected = mTestOutput[_trainingIndex];
+            _guiText = String.Format("{0}. OUTPUT: {1}, EXPECTED: {2}", _trainingIndex, GetTag(prediction), GetTag(expected));
+            SetScreenText(_guiText);
+        }
+
+        if (prediction == expected)
+        {
+            _correctClassification++;
+        }
+
+        _trainingIndex++;
+
+        if (_trainingIndex == mTestInput.Count)
+        {
+            var percent = ((float) _correctClassification / _trainingIndex) * 100;
+            Debug.Log(String.Format("CORRECT: {0}%", percent));
+        }
+
+        SecondaryGantry.SetAudioDesired(prediction == 1);
+    }
+
+
+    private void SetScreenText(string value)
+    {
+        Screen.text = value;
+    }
+
+
+    public void Run() {
+
+        // Access directories containing the training data. 
+        var desiredInfo = new DirectoryInfo(DESIRED_AUDIO_PATH);
+        var undesiredInfo = new DirectoryInfo(UNDESIRED_AUDIO_PATH);
+
+        // Extracts all the audio data into <FileInfo, Signal> objects.
+        _desiredData = DecodeTestData(desiredInfo);
+        _undesiredData = DecodeTestData(undesiredInfo);
+        _desiredAudio = GetSignalArray(_desiredData);
+        _undesiredAudio = GetSignalArray(_undesiredData);
+
+        // Process the signals and set their features. 
+        Debug.Log("Processing Desired Audio Signals");
+        ProcessSignals(_desiredAudio, DESIRED);
+        Debug.Log("Processing Undesired Audio Signals");
+        ProcessSignals(_undesiredAudio, UNDESIRED);
+        
+        double[][] trainingInput = mTrainingInput.ToArray();
+        int[] trainingOutput = mTrainingOutput.ToArray();       
+        
+        Debug.Log(String.Format("Size of Input: {0}", trainingInput.Length));
+        DecisionVariable[] variables =
+        {
+            new DecisionVariable("RMS", DecisionVariableKind.Continuous),
+            new DecisionVariable("MAX", DecisionVariableKind.Continuous),
+            new DecisionVariable("MOD", DecisionVariableKind.Continuous),
+            new DecisionVariable("ENERGY", DecisionVariableKind.Continuous)
+        };
+        
+        // Create the forest learning algorithm
+        var teacher = new RandomForestLearning(variables)
+        {
+            NumberOfTrees = 20, // use 20 trees in the forest
+        };
+
+        // Finally, learn a random forest from data
+        _forest = teacher.Learn(trainingInput, trainingOutput);
+    }
+
+    private string GetTag(int tag)
+    {
+        switch (tag)
+        {
+                case 1:
+                    return "Desired";
+                case 0:
+                    return "Undesired";
+        }
+
+        return "Error";
+    }
+
+
+    private void ProcessSignals(WaveStream[] signals, int desired)
+    {
+        
+        int maxTrainingVal = (int) Math.Ceiling(signals.Length * ((TRAINING_PERCENTAGE)/ 100));
+        Debug.Log(String.Format("Training Value: {0}", maxTrainingVal));
+        
+        for (int i = 0; i < signals.Length; i++)
+        {
+            var signal = signals[i];                
+            var values = ProcessSignal(signal);
+            var rms = values[0];
+            var max = values[1];
+            var average = values[2];
+            var spectral = values[3];
+            var energy = values[4];
+
+            var mod = average * (spectral);
+            double[] trainOn = {rms, max, mod, energy};
+            Debug.Log(String.Format("RMS: {0}\tMAX: {1}\tModified: {2}\tDESIRED: {3}, ENERGY: {4}", rms, max, mod, desired, energy));
+
+            if ( i >= maxTrainingVal)
+            {
+                mTestInput.Add(trainOn);
+                mTestOutput.Add(desired);
+            }
+            else
+            {
+                mTrainingInput.Add(trainOn);
+                mTrainingOutput.Add(desired);
+            }
+        }
+    }
+
+    private double[] ProcessSignal(WaveStream signal)
+    {
+        var len = (double) signal.Length;
+        var sampleRate = (double) signal.WaveFormat.SampleRate;
+        return new double[] {len, sampleRate, 0.0, 0.0, 0.0};
+    }
+
+    private Accord.Tuple<WaveStream, FileInfo>[] DecodeTestData(DirectoryInfo inputData) {
+        var signalList = new List<Accord.Tuple<WaveStream, FileInfo>>();
+        var files = inputData.GetFiles();
+        
+        foreach (var file in files) 
+        {
+            if (file.Extension == ".WAV")
+            {
+                WaveStream stream = new WaveFileReader(file.FullName);
+                var tuple = new Accord.Tuple<WaveStream, FileInfo>(stream, file);
+                signalList.Add(tuple);
+            }
+            
+        }
+        
+        return signalList.ToArray();
+    }
+
+    private WaveStream[] GetSignalArray(Accord.Tuple<WaveStream, FileInfo>[] list) {
+        List<WaveStream> signals = new List<WaveStream>();
+        foreach (var tuple in list) {
+            var signal = tuple.Item1;
+            signals.Add(tuple.Item1);
+        }
+        return signals.ToArray();
+    }
+}
--- Assets/Scripts/SecondaryGantry.cs
+++ Assets/Scripts/SecondaryGantry.cs
@@ -1,61 +1,230 @@
-﻿using UnityEngine;
+using System;
+using UnityEngine;
 using System.Collections;
 
 public class SecondaryGantry : MonoBehaviour
 {
-    private string SECONDARY_GANTRY_TAG = "secondaryGantry";
-    private string SECONDARY_GANTRY_Z_NEG_TAG = "secondaryGantryBottom";
-    private string SECONDARY_GANTRY_Z_POS_TAG = "secondaryGantryTop";
+	public float moveSpeed;
+	
+    // Tags for identifying the GameObjects in the simulation
+	private readonly string SECONDARY_GANTRY0 = "secondaryGantry0";
+	private readonly string SECONDARY_GANTRY1 = "secondaryGantry1";
+	private readonly string SECONDARY_GANTRY2 = "secondaryGantry2";
+	private readonly string SECONDARY_GANTRY3 = "secondaryGantry3";
+	private readonly string SECONDARY_GANTRY4 = "secondaryGantry4";
+	
+	private readonly string _SECTION0 = "section0";
+	private readonly string _SECTION1 = "section1";
+	private readonly string _SECTION2 = "section2";
+	private readonly string _SECTION3 = "section3";
+	
+	private GameObject _section0;
+	private GameObject _section1;
+	private GameObject _section2;
+	private GameObject _section3;
 
-    private GameObject[] mSecondaryGantryList;
-    private GameObject mSecondaryGantryPosBound;
-    private GameObject mSecondaryGantryNegBound;
+	private static bool _isAudioDesired = true;
+	
+	//this will be 5 when we get the ends moving
+	private int PRIMARY_GANTRY_SECTIONS = 4;
+	private int PRIMARY_GANTRY_BEAMS = 3; 
+	
+    // Secondary Gantry objects
+	private GameObject[] _gantry0;
+	private GameObject[] _gantry1;
+	private GameObject[] _gantry2;
+	private GameObject[] _gantry3;	
 
+	private Vector3 _topBound;
+	private Vector3 _lowBound;
+
+	// Corresponds to each gantry 0-4
+	private readonly Vector3[] _bottomStatePositions = {
+		new Vector3(9, -0.5f, 10.5f), 
+		new Vector3(9, -0.5f, 8.5f), 
+		new Vector3(9, -0.5f, 6.5f), 
+		new Vector3(9, -0.5f, 4.5f), 
+		new Vector3(9, -0.5f, 2.5f)
+	};
+
+	private readonly Vector3[] _balancedState =
+	{
+		new Vector3(9, -0.5f, 10.5f),
+		new Vector3(9, -0.5f, 5f),
+		new Vector3(9, -0.5f, 0f),
+		new Vector3(9, -0.5f, -5f),
+		new Vector3(9, -0.5f, -10f),
+	};
+	
+	// Corresponds to each gantry 0-4
+	private readonly Vector3[] _topStatePositions = {
+		new Vector3(9, -0.5f, -2f), 
+		new Vector3(9, -0.5f, -4f), 
+		new Vector3(9, -0.5f, -6f), 
+		new Vector3(9, -0.5f, -8f), 
+		new Vector3(9, -0.5f, -10f)
+	};
+	
+	// Corresponds to each gantry 0-4
+	private readonly Vector3[] _splitTopHeavy = {
+		new Vector3(9, -0.5f, 10.5f), 
+		new Vector3(9, -0.5f, 8.5f), 
+		new Vector3(9, -0.5f, -6f), 
+		new Vector3(9, -0.5f, -8f), 
+		new Vector3(9, -0.5f, -10f)
+	};
+	
+	
+	
+ 	private GameObject[][] mSecondaryGantryList;
+	private GameObject mPrimaryGantryPosBound;
+	private GameObject mPrimaryGantryNegBound;
+
+	private Vector3[][] mMovementList;
+    private float[][] mSpeedList;
     private float zBoundPos;
     private float zBoundNeg;
-
+	private float speedOffset = 0.5f;
     Vector3 beamMoveVector;
 
 
-	// Use this for initialization
-	void Start() {
-		mSecondaryGantryList = GameObject.FindGameObjectsWithTag(SECONDARY_GANTRY_TAG);
-        mSecondaryGantryPosBound = GameObject.FindGameObjectWithTag(SECONDARY_GANTRY_Z_POS_TAG);
-        mSecondaryGantryNegBound = GameObject.FindGameObjectWithTag(SECONDARY_GANTRY_Z_NEG_TAG);
-        beamMoveVector = new Vector3(0, 0, 1);
-
-        zBoundPos = mSecondaryGantryPosBound.transform.localPosition.z;
-        zBoundNeg = mSecondaryGantryNegBound.transform.localPosition.z;
+	GameObject[] GetSecondaryGantries(GameObject section)
+	{
+		var gantry = new GameObject[5];
+		GameObject.FindWithTag(SECONDARY_GANTRY0);
+		gantry[0] = section.transform.GetChild(0).gameObject;
+		gantry[1] = section.transform.GetChild(1).gameObject;
+		gantry[2] = section.transform.GetChild(2).gameObject;
+		gantry[3] = section.transform.GetChild(3).gameObject;
+		gantry[4] = section.transform.GetChild(4).gameObject;
+		
+		return gantry;
 	}
+
+	// Use this for initialization
+	void Start(){
+		
+		// Each of the sections containing 5 secondary gantrys
+		_section0 = GameObject.FindWithTag(_SECTION0);
+		_section1 = GameObject.FindWithTag(_SECTION1);
+		_section2 = GameObject.FindWithTag(_SECTION2);
+		_section3 = GameObject.FindWithTag(_SECTION3);
+		
+		// Arrays to control the secondary gantrys in each section.
+		_gantry0 = GetSecondaryGantries(_section0);
+		_gantry1 = GetSecondaryGantries(_section1);
+		_gantry2 = GetSecondaryGantries(_section2);
+		_gantry3 = GetSecondaryGantries(_section3);
+		
+        mPrimaryGantryPosBound = GameObject.FindGameObjectWithTag(SECONDARY_GANTRY0);
+        mPrimaryGantryNegBound = GameObject.FindGameObjectWithTag(SECONDARY_GANTRY4);
+
+		_topBound = mPrimaryGantryPosBound.transform.localPosition;
+		_lowBound = mPrimaryGantryNegBound.transform.localPosition;
+		
+		print(String.Format("Top Bound: {0}", _topBound));
+		print(String.Format("Low Bound: {0}", _lowBound));
+
+
+        beamMoveVector = new Vector3(0, 0, 1);
+        zBoundPos = mPrimaryGantryPosBound.transform.localPosition.z;
+		zBoundNeg = mPrimaryGantryNegBound.transform.localPosition.z;
+		print (zBoundNeg);
+		print(zBoundPos);
+		mMovementList = new Vector3[PRIMARY_GANTRY_SECTIONS][];
+		mSpeedList = new float[PRIMARY_GANTRY_SECTIONS][];
+
+		for (int i = 0; i < mMovementList.Length; i++) {
+			mMovementList[i] = new Vector3[3];
+			mSpeedList [i] = new float[3];
+		}
+
+        for (int i = 0; i < mMovementList.Length; i++){
+			for (int j = 0; j < mMovementList [0].Length; j++) {
+				mMovementList[i][j] = beamMoveVector;
+				beamMoveVector *= -1;
+			}
+        }
+		for (int i = 0; i < mMovementList.Length; i++) {
+			SetObjectSpeed (i);
+		}
+
+	}
+
+	
+	void Move(GameObject[] gantryList, Vector3[] directions)
+	{
+		float step = moveSpeed * Time.deltaTime;
+				
+		for (int i = 0; i < gantryList.Length; i++)
+		{
+			var gantry = gantryList[i];
+			var currentPos = gantry.transform.localPosition;
+			var target = directions[i];
+			gantry.transform.localPosition = Vector3.MoveTowards(currentPos, target, step);
+		}
+	}
+	
 
 	// Update is called once per frame
-	void Update()
+	void FixedUpdate(){
+		if (_isAudioDesired)
+		{
+			Move(_gantry0, _balancedState);
+			Move(_gantry1, _balancedState);
+			Move(_gantry2, _balancedState);
+			Move(_gantry3, _balancedState);
+		}
+		else
+		{
+			Move(_gantry0, _bottomStatePositions);
+			Move(_gantry1, _bottomStatePositions);
+			Move(_gantry2, _topStatePositions);
+			Move(_gantry3, _topStatePositions);
+		}
+
+	}
+
+	private bool CheckBounds(int index, GameObject[] gantryList){
+        bool switchDirections = false;
+		GameObject obj = gantryList[0];
+
+		for (int i = 1; i < mSecondaryGantryList[index].Length; i++){
+			GameObject primaryGantry = mSecondaryGantryList[index][i];
+            if (obj.transform.localPosition.z < zBoundNeg + 2){
+				return true;
+            }
+			if (primaryGantry.transform.localPosition.z > zBoundPos - 2){
+				return true;
+            }
+			float difference = Mathf.Abs(primaryGantry.transform.localPosition.z - obj.transform.localPosition.z);
+            if ( difference < 2){
+				return true;
+            }
+            obj = primaryGantry;
+        }
+        return switchDirections;
+    }
+
+    private void SwitchDirections(int section, GameObject[] gantryList){
+		if (CheckBounds (section, gantryList)) {
+			for (int i = 0; i < mSecondaryGantryList[section].Length; i++) {
+				mMovementList [section][i] *= -1;
+			}
+			SetObjectSpeed (section);
+		}
+    }
+
+	private void SetObjectSpeed(int index){
+		System.Random rand = new System.Random();
+        for (int i = 0; i < mSpeedList[index].Length; i++){
+			float speed = (float)(.5 + speedOffset);
+			mSpeedList [index][i] = speed;
+		}
+	}
+
+	public static void SetAudioDesired(bool isDesired)
 	{
-		foreach (GameObject gameobject in mSecondaryGantryList) {
-			gameobject.transform.Translate(beamMoveVector * (3 * Time.deltaTime));
-		}
-		SwitchDirections();
-	}
-
-	private bool CheckBounds() {
-		bool switchDirections = false;
-		foreach (GameObject secondaryGantry in mSecondaryGantryList) {
-            
-            if (secondaryGantry.transform.localPosition.z >= zBoundPos - 1) {
-				switchDirections = true;
-			}
-
-            if (secondaryGantry.transform.localPosition.z >= zBoundNeg + 1) {
-				switchDirections = true;
-			}
-
-		}
-		return switchDirections;
-	}
-
-	private void SwitchDirections(){
-		if (CheckBounds()) {
-			beamMoveVector *= -1;
-		}
+		_isAudioDesired = isDesired;
 	}
 }
