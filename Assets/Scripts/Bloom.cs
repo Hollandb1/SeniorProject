80bd79f859c8389f64f0c8fbdf7bb9e5798cf65a
Hollandb1 <Hollandb1@wit.edu>
2017-08-03 12:50:01 -0400

    weatherStates Added

--- Assets/Scripts/Bloom.cs
+++ Assets/Scripts/Bloom.cs
@@ -1,68 +1,140 @@
-﻿﻿using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using System;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(MeshRenderer))]
 public class Bloom : MonoBehaviour
 {
-    static int framecount;
-    bool forwards = true;
-    public Mesh[] meshes;
-    MeshFilter[] meshFilters;
-    GameObject[] flowers;
-    GameObject[] objects;
-    int index;
-    void Start()
-    {
-        framecount = 0;
-        index = 0;
-        objects = GameObject.FindGameObjectsWithTag("mesh");
-        var MyList = GameObject.FindGameObjectsWithTag("mesh").OrderBy(go => Convert.ToInt32(go.name.Substring(4))).ToList();
+	public enum WeatherStates
+	{
+		Sunny,
+		Rain,
+		Night,
+		PingPong
+	};
+	public WeatherStates weatherState;
+	bool forwards = true;
+	Mesh[] meshes;
+	MeshFilter[] meshFilters;
+	GameObject[] flowers;
+	GameObject[] objects;
+	int positionIndex;
 
-        meshes = new Mesh[objects.Length];
-        int i = 0;
-        foreach (GameObject obj in MyList){
-            meshes[i] = obj.GetComponent<MeshFilter>().mesh;
-            i++;
-        }
-        i = 0;
-        flowers = GameObject.FindGameObjectsWithTag("Flower");
-        print(flowers.Length);
+	void Start()
+	{
+		positionIndex = 0;
+		objects = GameObject.FindGameObjectsWithTag("mesh");
+		var MyList = GameObject.FindGameObjectsWithTag("mesh").OrderBy(go => Convert.ToInt32(go.name.Substring(4))).ToList();
 
-        meshFilters = new MeshFilter[flowers.Length];
-        foreach (GameObject flower in flowers){
-			meshFilters[i] = flower.GetComponent<MeshFilter>();
-            i++;
+		meshes = new Mesh[objects.Length];
+		int i = 0;
+		foreach (GameObject obj in MyList)
+		{
+			meshes[i] = obj.GetComponent<MeshFilter>().mesh;
+			i++;
 		}
-    }
+		i = 0;
+		flowers = GameObject.FindGameObjectsWithTag("Flower");
+		print(flowers.Length);
 
-    void Update()
-    {
-        foreach(MeshFilter meshFilter in meshFilters)
-        {
-            meshFilter.mesh = meshes[index];
-        }
-            nextPosition(ref index, ref forwards);
-    }
+		meshFilters = new MeshFilter[flowers.Length];
+		foreach (GameObject flower in flowers)
+		{
+			meshFilters[i] = flower.GetComponent<MeshFilter>();
+			i++;
+		}
+	}
 
-    public static void nextPosition(ref int index, ref bool forwards)
-    {
-        if (index == 121)
-        {
-            forwards = false;
-            framecount = 0;
-        }
-        if (index == 0){
-            forwards = true;
-        }
-        if (forwards == true){
-            index++;
-        }
-        else{
-            index--;
-        }
-    }
+	void Update()
+	{
+		if (weatherState == WeatherStates.Night)
+		{
+			bloomNight();
+		}
+		else if (weatherState == WeatherStates.Sunny)
+		{
+			bloomSunny();
+		}
+		else if (weatherState == WeatherStates.Rain)
+		{
+			bloomRain();
+		}
+		else
+		{
+			pingPong();
+		}
+	}
+
+	private void bloomRain()
+	{
+		if (positionIndex < 122)
+		{
+			positionIndex++;
+			foreach (MeshFilter meshFilter in meshFilters)
+			{
+				meshFilter.mesh = meshes[positionIndex];
+			}
+		}
+	}
+
+	private void bloomSunny()
+	{
+		if (positionIndex < 61)
+		{
+			positionIndex++;
+			foreach (MeshFilter meshFilter in meshFilters)
+			{
+				meshFilter.mesh = meshes[positionIndex];
+			}
+		}
+		else if (positionIndex > 61)
+		{
+			positionIndex--;
+			foreach (MeshFilter meshFilter in meshFilters)
+			{
+				meshFilter.mesh = meshes[positionIndex];
+			}
+		}
+	}
+
+	private void bloomNight()
+	{
+		if (positionIndex > 0)
+		{
+			positionIndex--;
+			foreach (MeshFilter meshFilter in meshFilters)
+			{
+				meshFilter.mesh = meshes[positionIndex];
+			}
+		}
+	}
+
+	public void pingPong()
+	{
+
+		if (positionIndex == 122)
+		{
+			forwards = false;
+		}
+		if (positionIndex == 0)
+		{
+			forwards = true;
+		}
+		if (forwards == true)
+		{
+			positionIndex++;
+		}
+		else
+		{
+			positionIndex--;
+		}
+		foreach (MeshFilter meshFilter in meshFilters)
+		{
+			meshFilter.mesh = meshes[positionIndex];
+		}
+	}
 
 }
